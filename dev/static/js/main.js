309cf6f3fc3740f07e8a74ac1475b8eb197c3767
Mher-777 <mherjan2003@mail.ru>
2020-10-15 16:25:38 +0400

    added new section recommend

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -13,12 +13,28 @@ $(function () {
         $('.category__slider').slick({
             slidesToShow: 7,
             slidesToScroll: 1,
+            touchThreshold: 40,
+            prevArrow: '<button type="button" class="arrow arrow--left"><img class="arrow__icon" src="static/images/common/arrow-left.svg" alt=""></button>',
+            nextArrow: '<button type="button" class="arrow arrow--right"><img class="arrow__icon" src="static/images/common/arrow-right.svg" alt=""></button>'
+        })
+        $('.product__head-slider').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: true,
+            dotsClass: 'pagination product__pagination pagination--white',
+            infinite: false,
+            touchThreshold: 40,
+        })
+        $('.recommend__slider').slick({
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            swipe: false,
             prevArrow: '<button type="button" class="arrow arrow--left"><img class="arrow__icon" src="static/images/common/arrow-left.svg" alt=""></button>',
             nextArrow: '<button type="button" class="arrow arrow--right"><img class="arrow__icon" src="static/images/common/arrow-right.svg" alt=""></button>'
         })
     }
-    mainSliders()
-    let sliderSpace = () => {
+    const sliderSpace = () => {
         $('[data-space]').each(function () {
             let $this = $(this),
                 $space = $this.attr('data-space');
@@ -30,9 +46,22 @@ $(function () {
                 marginLeft: -$space + 'px',
                 marginRight: -$space/2 + 'px'
             })
+            return false
         });
     }
+    const toggle = (parent, elem, elemClass) => {
+        let box = $(parent)
+        const item = $(elem)
+        box.each(function () {
+            const boxThis = $(this)
+            $(this).find(item).on('click', function () {
+                boxThis.find(item).toggleClass(elemClass)
+            })
+        })
 
+    }
+    toggle('.product__head', '.js-toggle', 'icon-heart--active')
+    mainSliders()
     sliderSpace()
 })
 function browser() {
@@ -41,3 +70,27 @@ function browser() {
     }
 }
 browser()
+const counter = function (input) {
+    let inputEl = document.querySelector(input)
+    let btns = document.querySelectorAll( '.counter__btn' );
+    Array.prototype.forEach.call(btns, function (item) {
+        item.addEventListener('click', counterState)
+    });
+
+    function counterState() {
+        let dir = this.dataset.direction,
+            currentValue = inputEl.value;
+        dir === 'plus' ? counterPlus(inputEl, currentValue) : counterMinus(inputEl, currentValue);
+    }
+
+    const counterPlus = (el, val) => {
+        el.value = +val + 1;
+    };
+
+    const counterMinus = (el, val) => {
+        if (val - 1) el.value = +val - 1;
+    };
+
+};
+counter('.counter__input');
+
