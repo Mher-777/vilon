8dceb821d911e063673cf2473e5094a86c175282
Mher-777 <mherjan2003@mail.ru>
2020-10-15 13:16:03 +0400

    added new section category

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,14 +1,39 @@
-
 $(function () {
     svg4everybody({});
-    $('.slider__carousel').slick({
-        fade: true,
-        arrows: false,
-        dots: true,
-        autoplay: true,
-        touchThreshold: 40,
-        dotsClass: 'pagination slider__pagination'
-    })
+    const mainSliders = () => {
+        $('.slider__carousel').slick({
+            fade: true,
+            arrows: false,
+            dots: true,
+            autoplay: true,
+            touchThreshold: 40,
+            dotsClass: 'pagination slider__pagination',
+            cssEase: 'linear'
+        })
+        $('.category__slider').slick({
+            slidesToShow: 7,
+            slidesToScroll: 1,
+            prevArrow: '<button type="button" class="arrow arrow--left"><img class="arrow__icon" src="static/images/common/arrow-left.svg" alt=""></button>',
+            nextArrow: '<button type="button" class="arrow arrow--right"><img class="arrow__icon" src="static/images/common/arrow-right.svg" alt=""></button>'
+        })
+    }
+    mainSliders()
+    let sliderSpace = () => {
+        $('[data-space]').each(function () {
+            let $this = $(this),
+                $space = $this.attr('data-space');
+            $this.find('.slick-slide').css({
+                marginLeft: $space + 'px',
+                marginRight: $space + 'px'
+            });
+            $this.find('.slick-list').css({
+                marginLeft: -$space + 'px',
+                marginRight: -$space/2 + 'px'
+            })
+        });
+    }
+
+    sliderSpace()
 })
 function browser() {
     if ((navigator.userAgent.indexOf("MSIE") !== -1) || (!!document.documentMode === true)) {
